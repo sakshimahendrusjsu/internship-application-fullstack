59504cbc1018a3d1bbb20c534e531f7feecbda18
sakshimahendrusjsu <55038395+sakshimahendrusjsu@users.noreply.github.com>
2020-04-16 13:30:52 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const NAME = 'AB-TEST'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +8,65 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+const cookie = request.headers.get('cookie')
+
+let originalResponse = await fetch(new URL('https://cfw-takehome.developers.workers.dev/api/variants'));
+let originalBody = await originalResponse.json();           
+let jsonData = originalBody.variants;
+     if (cookie && cookie.includes(`${NAME}=zero`)) {
+       let response =  await fetch(jsonData[0])
+       return rewriter.transform(response)
+    } else if (cookie && cookie.includes(`${NAME}=one`)) {
+       let response =  await fetch(jsonData[1])
+       return rewriter2.transform(response)
+    } else{
+     if (Math.random() < 0.5){
+        let response = await fetch(jsonData[0])
+        response = new Response(response.body,response)
+        response.headers.append('Set-Cookie', `${NAME}=zero`)
+        return rewriter.transform(response)
+     }else{
+      let response = await fetch(jsonData[1])
+      response = new Response(response.body,response)
+      response.headers.append('Set-Cookie', `${NAME}=one`)
+      return rewriter2.transform(response)
+     }
+    }
 }
+
+class AttributeRewriter {
+  constructor(attributeName,old_val,new_val) {
+    this.attributeName = attributeName
+    this.old_val = old_val
+    this.new_val = new_val
+  }
+
+  element(element) {
+    const attribute = element.getAttribute(this.attributeName)
+    //  console.log("attribute:::",attribute);
+    if (attribute) {
+      element.setAttribute(
+        this.attributeName,
+        attribute.replace(this.old_val, this.new_val),
+      )
+    }
+  }
+}
+
+const rewriter = new HTMLRewriter()
+.on('a#url', new AttributeRewriter('href','https://cloudflare.com','https://google.com'))
+.on('title',{element(element){ element.setInnerContent('Cloudflare 1')}})
+.on('a#url', {element(element){ element.setInnerContent('Go to google.com')}})
+.on('h1#title',{element(element){ element.setInnerContent('Cloudflare 1')}})
+.on('p#description',{element(element){ element.setInnerContent('Developed by Sakshi Mahendru. Thankyou for visting us!!')}})
+
+
+const rewriter2 = new HTMLRewriter()
+.on('a#url', new AttributeRewriter('href','https://cloudflare.com','https://google.com'))
+.on('title',{element(element){ element.setInnerContent('Cloudflare 2')}})
+.on('a#url', {element(element){ element.setInnerContent('Go to google.com')}})
+.on('h1#title',{element(element){ element.setInnerContent('Cloudflare 2')}})
+.on('p#description',{element(element){ element.setInnerContent('Developed by Sakshi Mahendru. Have a great day!!')}})
+
+
+
